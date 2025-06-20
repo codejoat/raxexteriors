99952272ada91fc108abc5c895611edf537521fb
Christopher Lawyer <joat1979@gmail.com>
2025-06-20 16:34:23 -0500

    Added form and a bit of JS to control form submit, added a couple of styling points as well

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-document.getElementById("current-date").textContent = new Date().toLocaleDateString("en-US", {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric"
+window.addEventListener('DOMContentLoaded', function () {
+  const dateEl = document.getElementById("current-date");
+  if (dateEl) {
+    dateEl.textContent = new Date().toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  }
+
+  const form = document.querySelector('form');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      alert('Request Sent');
+      window.location.href = '../index.html';
+    });
+  }
 });
